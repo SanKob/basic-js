cce9b5283492645ad300796f94bc55a8faa77a06
San <sanek2184@rambler.ru>
2020-10-31 23:58:38 +0300

    feat: execude some tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam( members ) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let firstCharMembers = [];
+  members.forEach((el) => {
+  if (typeof el === 'string') {
+    firstCharMembers = firstCharMembers.concat(el.trim()[0].toUpperCase());
+  }
+ }) 
+    
+ return firstCharMembers.sort().join('');
 };
+
+
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let { repeatTimes, separator = '+', addition = '', additionRepeatTimes, 
+  additionSeparator = '|' } = options;
+        
+    let newStr = addition;
+   
+   for ( let i = 1; i < additionRepeatTimes; i++) {
+     newStr = newStr + additionSeparator + addition;
+   }
+   let result = str + newStr;
+    for (let i = 1; i < repeatTimes; i++) {
+      result = result + separator + str + newStr;
+    }
+    return result;
+ 
 };
   
